c7422dc98777217377e241a3849cede20af5d3b8
Evan Wilt <ev.wilt@gmail.com>
2018-07-24 19:11:48 -0400

    Added load, inc, and dec opcodes, removed illegal opcodes

--- src/instructions.rs
+++ src/instructions.rs
@@ -4,6 +4,16 @@ pub fn ld_reg_pair_reg_pair(src: &mut RegisterPair, dest: &mut RegisterPair) {
     dest.set_pair(src.get_pair());
 }
 
+pub fn inc_reg_pair(dest: &mut RegisterPair) {
+    let prev_pair = dest.get_pair();
+    dest.set_pair(prev_pair.wrapping_add(1));
+}
+
+pub fn dec_reg_pair(dest: &mut RegisterPair) {
+    let prev_pair = dest.get_pair();
+    dest.set_pair(prev_pair.wrapping_sub(1));
+}
+
 pub fn ld_u16_reg_pair(src: u16, dest: &mut RegisterPair) {
     dest.set_pair(src);
 }
